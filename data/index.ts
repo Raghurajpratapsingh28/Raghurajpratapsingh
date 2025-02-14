862bf203f85b8ad2bfc72759be7cacc01b41c49a
Raghuraj Pratap Singh <Raghurajpratap2810@gmail.com>
2025-02-14 15:06:15 +0530

    Adding Portfolio

---
+++ data/index.ts
@@ -0,0 +1,138 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    // { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a more Advanced AI based Code Editor",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "An AI based Code Editor",
+      des: "An code Editor that can compile and run your code and shows recommendation and also saves your code",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/Raghurajpratapsingh28/AI-based-code-editor",
+    },
+    {
+      id: 2,
+      title: "An Chat Application",
+      des: "Designed and developed a real-time chat application with instant messaging, user authentication, and multimedia sharing features for seamless communication",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/Raghurajpratapsingh28/Synapse",
+    },
+    
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Ye bahut acha admi hai ",
+      name: "Lodu Prasad",
+      title: "Director of Lund University",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Developer - Cyber Security Hackathon",
+      desc: "Assisted in the development of a Web Extension, to decrypt Cieser Cypher Text.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:"https://github.com/Raghurajpratapsingh28",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link:"https://x.com/raghuraj2810?s=21",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/raghuraj-pratap-singh-712811294/",
+    },
+  ];
